2091bc7e19510200369f06f0dcd6633abaed2565
Sean Lee <seanboramlee@gmail.com>
2017-01-22 16:34:35 -0800

    create project card container

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import DeveloperProjectCard from './DeveloperProjectCard'
 const data = require('../public/projects')
+
+const shows = data
+const puke = obj => {
+  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
+}
 
 class Developer extends React.Component {
   render() {
     return(
       <div>
         <h1>Developer muhhfucker</h1>
+        <DeveloperProjectCard></DeveloperProjectCard>
+          {puke(data)}
       </div>
     )
   }
---
+++ src/components/DeveloperProjectCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class DeveloperProjectCard extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Project Card goes here</p>
+      </div>
+    )
+  }
+}
+
+export default DeveloperProjectCard
