97f5739d28c587e3535ee64bf95641ba4d87b904
Sean Lee <seanboramlee@gmail.com>
2017-01-23 17:24:59 -0800

    modal component now updates based on click on parent component

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -3,8 +3,22 @@ import React from 'react'
 class ProjectModal extends React.Component {
   constructor(props) {
     super(props)
-    console.log(props);
+    this.renderModal = this.renderModal.bind(this);
+  }
 
+  componentDidMount() {
+    console.log('component did mount', this.props);
+    this.renderModal(this.props)
+  }
+  componentDidUpdate() {
+    console.log('component updated', this.props)
+    this.renderModal(this.props)
+  }
+
+  renderModal(props) {
+    if(props.isOpen === true) {
+      console.log('modal is open');
+    }
   }
   render() {
     return null
