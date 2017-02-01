75e77d00f5e00ff0c6ac9c57f142e122613e60ac
Sean Lee <seanboramlee@gmail.com>
2017-01-31 21:57:38 -0800

    create selectProfile actionCreator

--- src/reducers/reducer_profile.js
+++ src/reducers/reducer_profile.js
@@ -1,1 +1,10 @@
 const INITIAL_STATE = { profile: DEVELOPER, activeProject: null}
+
+export default function(state = INITIAL_STATE, action ) {
+  switch(action.type) {
+    case DEVELOPER_SELECTED:
+      return {state: action}
+    default:
+      return state;
+  }
+}
