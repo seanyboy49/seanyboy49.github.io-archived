2c80508b4b4fe70fc5237917f861837a7752b737
Sean Lee <seanboramlee@gmail.com>
2017-02-01 13:17:27 -0800

    clicking a link in the navigation component successfully calls the selectProfile actionCreator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ class Navigation extends React.Component{
     return(
       <div >
         <div className="nav-bar" style={{"backgroundColor": "#3498db"}}>
-            <div className="panel" onClick={() => this.props.selectProfile()}>Developer</div>
+            <div className="panel" onClick={() => this.props.selectProfile('Developer')}>Developer</div>
         </div>
         {this.props.children}
       </div>
