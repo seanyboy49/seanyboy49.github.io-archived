7e1c00ae68d40088a8353e7975622675b19f8f76
Sean Lee <seanboramlee@gmail.com>
2017-02-01 15:43:35 -0800

    handleClick to nav component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,11 +6,17 @@ import { selectProfile } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class Navigation extends React.Component{
+
+  handleClick(e) {
+    this.props.selectProfile(e.target.innerHTML)
+    console.log('handle click', e.target.innerHTML);
+  }
+
   render() {
     return(
       <div >
         <div className="nav-bar" style={{"backgroundColor": "#3498db"}}>
-            <a className="panel" onClick={() => this.props.selectProfile('Developer')}>Developer</a>
+            <a className="panel" onClick={this.handleClick.bind(this)}>Developer</a>
             <a className="panel" onClick={() => this.props.selectProfile('Developer')}>Designer</a>
             <a className="panel" onClick={() => this.props.selectProfile('Developer')}>Filmmaker</a>
         </div>
--- src/reducers/reducer_profile.js
+++ src/reducers/reducer_profile.js
@@ -2,11 +2,11 @@ import { PROFILE_SELECTED } from '../actions/index';
 
 
 
-export default function(state = 'DEVELOPER', action) {
-  console.log('INSIDE REDUCER PROFILE', action);
+export default function(state = 'Developer', action) {
+  console.log('INSIDE REDUCER PROFILE', action.payload);
   switch(action.type) {
     case PROFILE_SELECTED:
-      return action.payload;
+      return { state: action.payload};
   }
   return state;
 }
