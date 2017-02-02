17bcb07612bb730f8c464a89d5c7706157da272e
Sean Lee <seanboramlee@gmail.com>
2017-02-01 17:36:39 -0800

    add Modal Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
 import CardsContainer from './CardsContainer';
+import Modal from './Modal'
 
 export default class App extends Component {
+
+  ModalCondition() {
+    if(this.props.activeCard===null) {
+      return
+    } else {
+      <Modal />
+    }
+  }
+
   render() {
     return (
       <div>
         <Navigation />
         <CardsContainer />
+        {this.ModalCondition()}
       </div>
     )
   }
