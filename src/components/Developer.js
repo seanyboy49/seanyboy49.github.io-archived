0a53e346d8065c7e4eba9794d68b8bc738166cbf
Sean Lee <seanboramlee@gmail.com>
2017-02-09 11:40:11 -0800

    add DeveloperProjectCard component to Developer route

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import DeveloperProjectCard from './DeveloperProjectCard';
+const data = require('../public/projects')
+
+
+
 class Developer extends React.Component{
   render() {
     return(
-      <div>
-        Developer
+      <div className="Dev-Project-Card-Container">
+        {data.projects.map((project, index) => {
+          return(
+            <DeveloperProjectCard {...project} key={index} />
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
