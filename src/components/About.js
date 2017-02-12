ce64ba37feff3545aefbdceed2c54ce5d96b8868
Sean Lee <seanboramlee@gmail.com>
2017-02-11 18:17:59 -0800

    add icons to About page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends React.Component{
       <div className="about-container">
         <h1 className="header-content">Sean Lee</h1>
         <h2 className="subhead">A programmer, a designer and a filmmaker walk into a bar...</h2>
-        
+
         <section className="about-content">
           <p>It sounds like the beginning of a terrible joke, but it's actually my resume.</p>
           <p>A creative at heart with a passion for design and technology, I'm drawn towards projects that use bold, beautiful design choices to improve human/computer interaction.</p>
@@ -19,12 +19,13 @@ class About extends React.Component{
         </section>
 
             <ul className="key-stats">
-              <li>San Francisco, United States</li>
-              <li><a href="https://github.com/seanyboy49">https://github.com/seanyboy49 </a></li>
-              <li>Searching</li>
-              <li>Freelance</li>
-              <li>Contact</li>
-              <li>https://www.linkedin.com/in/seanboramlee</li>
+              <li> <img className="icon" src="src/public/icons/Icons_Location.png"></img>San Francisco, United States</li>
+              <li> <img className="icon" src="src/public/icons/Icons_GitHub.png"></img> <a href="https://github.com/seanyboy49">seanyboy49 </a></li>
+              <li><img className="icon" src="src/public/icons/Icons_Employed.png"></img>Searching</li>
+              <li> <img className="icon" src="src/public/icons/Icons_Freelance.png"></img>Freelance</li>
+              <li><img className="icon" src="src/public/icons/Icons_Contact.png"></img>Contact</li>
+              <li><img className="icon" src="src/public/icons/Icons_Resume.png"></img>Resume</li>
+              <li><img className="icon" src="src/public/icons/Icons_LinkedIn.png"></img> <a href="https://www.linkedin.com/in/seanboramlee">seanboramlee</a></li>
             </ul>
       </div>
     )
