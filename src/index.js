48469d28452a89cfeeb59453bebabe89e3db48eb
Sean Lee <sean@Seans-MacBook-Pro.local>
2022-12-22 20:17:22 -0800

    create react app

--- src/index.js
+++ src/index.js
@@ -1,44 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import reducers from './reducers';
-import promise from 'redux-promise'
-
-
-import App from './components/app'
-import Navigation from './components/Navigation'
-import Developer from './components/Developer'
-import Designer from './components/Designer'
-import Filmmaker from './components/Filmmaker'
-import About from './components/About'
-
-
-
-const routes = (
-  <Route path="/" component={Navigation}>
-    <IndexRoute component={Developer} />
-    <Route path="/developer" component={Developer}/>
-    <Route path="/designer" component={Designer} />
-    <Route path="/filmmaker" component={Filmmaker} />
-    <Route path="/about" component={About} />
-
-
-  </Route>
-)
-
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
-render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router
-      history={browserHistory}
-      routes={routes}
-       />
-
-  </Provider>
-  , document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
